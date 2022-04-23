2df08031eb80e4e8ce535babe9ae53a2f3d834c7
Speedair9 <98007142+Speedair9@users.noreply.github.com>
2022-04-23 20:00:13 +0700

    init code

---
+++ lookforcar/src/main/java/com/mmo/lookforcar/LookforcarApplication.java
@@ -0,0 +1,13 @@
+package com.mmo.lookforcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookforcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookforcarApplication.class, args);
+	}
+
+}
---
+++ lookforcar/src/test/java/com/mmo/lookforcar/LookforcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmo.lookforcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookforcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
